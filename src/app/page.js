9e1450cd76d85574c964a076b8397d811aadf2fd
Sushma Sarkar <123677590+SushmaSarkar26@users.noreply.github.com>
2024-02-25 00:53:38 +0530

    add file

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Basic Routing | Make New Page</h1>
+    </main>
+  )
+}
+
+
+
+
