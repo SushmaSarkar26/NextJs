2487f46ae824d0883787e55bc485b82366a738bb
Sushma Sarkar <123677590+SushmaSarkar26@users.noreply.github.com>
2024-02-28 12:52:37 +0530

    add Nested Routing file

---
+++ login/loginstudent/page.js
@@ -0,0 +1,10 @@
+const Loginstudent =()=>{
+    return(
+        <div>
+            <h1>Login Page for Student</h1>
+        </div>
+    )
+}
+
+
+export default Loginstudent;
---
+++ login/loginteacher/page.js
@@ -0,0 +1,10 @@
+const Loginteacher =()=>{
+    return(
+        <div>
+            <h1>Login Page for Teacher</h1>
+        </div>
+    )
+}
+
+
+export default Loginteacher;
