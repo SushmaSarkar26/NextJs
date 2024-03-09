06673e1ac5dbc742d8889c3ad97a0d0eae028849
Sushma Sarkar <123677590+SushmaSarkar26@users.noreply.github.com>
2024-03-09 23:04:52 +0530

    add file

---
+++ productlist/page.js
@@ -0,0 +1,24 @@
+"use client"
+import { useEffect, useState } from 'react'
+
+export default function Page(){
+    const [product, setProduct] = useState([])
+
+    useEffect(async ()=>{
+        let data = await fetch("https://dummyjson.com/products");
+        data = await data.json();
+        console.log(data);
+        setProduct(data.products)
+    }, [])
+
+    return(
+        <div>
+            <h1>Product List</h1>
+            {
+                product.map((item)=>(
+                    <h3>Product : {item.title}, Price: {item.price}</h3>
+                ))
+            }
+        </div>
+    )
+}
